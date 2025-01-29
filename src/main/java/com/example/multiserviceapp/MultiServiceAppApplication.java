6deca0801929c5723ed1d020cd59061b3632173e
Chintakayala Rao <chintakayala.rao@nagarro.com>
2025-01-29 08:31:27 +0530

    Initial commit

---
+++ src/main/java/com/example/multiserviceapp/MultiServiceAppApplication.java
@@ -0,0 +1,13 @@
+
+package com.example.multiserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiServiceAppApplication.class, args);
+    }
+}
